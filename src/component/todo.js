da845829dee8ca3eea1683b392b63bcff8c9f299
rohitstar <rohitchauhan140398@gmail.com>
2021-04-29 11:18:20 +0530

    second commit

--- src/component/todo.js
+++ src/component/todo.js
@@ -15,21 +15,52 @@ const getLocalItmes = () => {
 const Todo = () => {
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState(getLocalItmes());
+  const [taggleButton, setTaggleButton] = useState(true);
+  const [isEditItem, setIsEditItem] = useState(null);
 
   const addItem = () => {
     if (!inputData) {
+      console.log("msdks");
+    } else if (inputData && !taggleButton) {
+      setItems(
+        items.map((element) => {
+          if (element.id === isEditItem) {
+            return { ...element, name: inputData };
+          }
+          return element;
+        })
+      );
+
+      setTaggleButton(true);
+      setInputData("");
+      setIsEditItem(null);
     } else {
-      setItems([...items, inputData]);
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
       setInputData("");
     }
   };
 
-  const deleteItem = (id) => {
-    console.warn("check the items", id);
-    const updateditems = items.filter((element, index) => {
-      return index !== id;
+  const deleteItem = (index) => {
+    // console.warn("check the items", id);
+    const updateditems = items.filter((element) => {
+      return index !== element.id;
     });
     setItems(updateditems);
+  };
+
+  const editItem = (id) => {
+    const newEditItem = items.find((element, index) => {
+      return element.id === id;
+    });
+    // setItems(newEditItem);
+    console.warn("item", newEditItem);
+    setTaggleButton(false);
+    setInputData(newEditItem.name);
+    setIsEditItem(id);
   };
 
   const removeAll = () => {
@@ -55,45 +86,83 @@ const Todo = () => {
             value={inputData}
             onChange={(e) => setInputData(e.target.value)}
           />
-          <button
-            style={{
-              padding: "15px",
-              border: "none",
-              borderRadius: "25px",
-              fontSize: "18px",
-              fontWeight: "bold",
-              marginLeft: "20px",
-            }}
-            //   className="fa fa-plus add-btn"
-            title="Add Item"
-            onClick={addItem}
-          >
-            Go
-          </button>
+          {taggleButton ? (
+            <button
+              style={{
+                padding: "15px",
+                border: "none",
+                borderRadius: "25px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}
+              //   className="fa fa-plus add-btn"
+              title="Add Item"
+              onClick={addItem}
+            >
+              Go
+            </button>
+          ) : (
+            <button
+              style={{
+                padding: "15px",
+                border: "none",
+                borderRadius: "25px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                marginLeft: "20px",
+              }}
+              //   className="fa fa-plus add-btn"
+              title="Update Item"
+              onClick={addItem}
+            >
+              Edit
+            </button>
+          )}
         </div>
 
         <div className="showItems">
-          {items.map((element, index) => {
+          {items.map((element) => {
             return (
-              <div className="eachItem" key={index}>
-                <h3>{element}</h3>
-                <button
-                  style={{
-                    padding: "5px 10px",
-                    border: "1px solid #fff",
-                    borderRadius: "25px",
-                    fontSize: "14px",
-                    fontWeight: "bold",
-                    marginLeft: "20px",
-                    backgroundColor: "red",
-                    color: "#fff",
-                  }}
-                  // className="far fa-trash-alt add-btn"
-                  title="Delete Item"
-                  onClick={() => deleteItem(index)}
-                >
-                  Trash
-                </button>
+              <div className="eachItem" key={element.id}>
+                <h3>{element.name}</h3>
+                <div>
+                  <button
+                    style={{
+                      padding: "5px 10px",
+                      border: "1px solid #fff",
+                      borderRadius: "25px",
+                      fontSize: "14px",
+                      fontWeight: "bold",
+                      marginLeft: "20px",
+                      backgroundColor: "green",
+                      color: "#fff",
+                    }}
+                    // className="far fa-trash-alt add-btn"
+                    title="Edit Item"
+                    onClick={() => editItem(element.id)}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    style={{
+                      padding: "5px 10px",
+                      border: "1px solid #fff",
+                      borderRadius: "25px",
+                      fontSize: "14px",
+                      fontWeight: "bold",
+                      marginLeft: "5px",
+                      backgroundColor: "red",
+                      color: "#fff",
+                    }}
+                    // className="far fa-trash-alt add-btn"
+                    title="Delete Item"
+                    onClick={() => deleteItem(element.id)}
+                  >
+                    Trash
+                  </button>
+                </div>
               </div>
             );
           })}
